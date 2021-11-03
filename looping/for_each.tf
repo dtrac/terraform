b1897bc7865c6cf7e8ab8d5bc16a948482334465
dtrac <dtracey@hotmail.co.uk>
2021-11-03 18:30:49 +0000

    add complex object example

--- looping/for_each.tf
+++ looping/for_each.tf
@@ -17,6 +17,41 @@ resource "azurerm_resource_group" "rg_map" {
   location = each.value
 }
 
+# Using a more complex Map/Object
+
+variable "complex_obj" {
+  type = map(object({
+    location = string
+    name     = string
+    tags     = map(string)
+  }))
+  default = {
+    "rg03" = {
+      location = "uksouth"
+      name     = "rg03"
+      tags = {
+        "key" = "value"
+      }
+    }
+    "rg04" = {
+      location = "ukwest"
+      name     = "rg04"
+      tags = {
+        "key" = "value"
+      }
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg_complex_map" {
+  for_each = var.complex_obj
+
+  name     = each.value["name"]
+  location = each.value["location"]
+
+  tags = each.value["tags"]
+}
+
 # Using a set (list) of strings
 
 variable "name" {
